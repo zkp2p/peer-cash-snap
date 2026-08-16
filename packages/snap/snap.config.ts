f8986c135f994e071e59cf1672afa22f3f875fc1
Richard <richardliang2015@u.northwestern.edu>
2026-08-16 16:52:32 -0400

    Initial commit

---
+++ packages/snap/snap.config.ts
@@ -0,0 +1,14 @@
+import type { SnapConfig } from '@metamask/snaps-cli';
+import { resolve } from 'path';
+
+const config: SnapConfig = {
+  input: resolve(__dirname, 'src/index.tsx'),
+  server: {
+    port: 8080,
+  },
+  polyfills: {
+    buffer: true,
+  },
+};
+
+export default config;
